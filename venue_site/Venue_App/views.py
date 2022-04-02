a7edf9fc500a21ef0aa9d08a0a53226cd034e20f
DishantWakalkar <dishantwakalkar@gmail.com>
2022-04-02 20:47:50 +0530

    Some Update

--- venue_site/Venue_App/views.py
+++ venue_site/Venue_App/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render (render,'Web_App/home.html')
+    
--- venue_site/venue_site/urls.py
+++ venue_site/venue_site/urls.py
@@ -13,8 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+from django.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
